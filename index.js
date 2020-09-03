4f875734f6cbbb791ac02db9f448872066812e2b
SuzukiDaishi <zukky.rikugame@gmail.com>
2020-09-04 07:35:53 +0900

    CORSの回避

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ app.use(express.urlencoded({ extended: true }))
 
 // CORSを許可
 app.use((req, res, next) => {
+    // FIXME: セキュリティなんとかせねば
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', '*')
     next()
 })
 
