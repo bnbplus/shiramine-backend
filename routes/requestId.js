8a33e7a360099c46a348ae0ad3f3528c4a4020e9
nunotani123 <nunotani123@gmail.com>
2020-09-10 13:37:16 +0900

    requestIdの作成

---
+++ routes/requestId.js
@@ -0,0 +1,32 @@
+const models = require('../models/index')
+
+module.exports = async (req, res) => {
+
+    let record = null
+    try {
+        record　= await models.request.findOne({ where: { id: req.params.id } })
+        if (record === null) {
+            return res.status(400).json({
+                success: false,
+                message: 'your id is wrong'
+            }) 
+        }
+    } catch (err) {
+        // データベースの故障
+        return res.status(500).json({
+            success: false,
+            message: 'database is corrupted.'
+        })
+    }
+
+    // 必要なデータのみ抽出
+    record = {
+        id: record.id,
+        information: record.information
+    }
+
+    return res.status(200).json({
+        success: true,
+        record: record
+    })
+}
